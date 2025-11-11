de71719990c7dfd7e8a0a07519d8863d86f2e677
PRECIT <sbjswapnil@gmail.com>
2025-11-11 17:14:35 +0530

    final 1 cpp

--- SortStudentData.cpp
+++ SortStudentData.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstring>
-#include <cstdlib>
 using namespace std;
 
 struct Student {
@@ -9,62 +8,47 @@ struct Student {
     float cgpa;
 };
 
-void addStudent(Student*& students, int& size) {
-    students = (Student*)realloc(students, (size + 1) * sizeof(Student));
-    cout << "Enter ID: ";
-    cin >> students[size].id;
-    cout << "Enter Name: ";
-    cin >> students[size].name;
-    cout << "Enter CGPA: ";
-    cin >> students[size].cgpa;
-    size++;
-}
-
-void displayStudents(Student* students, int size) {
-    cout << "\nID\tName\tCGPA\n";
-    for (int i = 0; i < size; i++)
-        cout << students[i].id << "\t" << students[i].name << "\t" << students[i].cgpa << endl;
-}
-
-void bubbleSortByName(Student* students, int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (strcmp(students[j].name, students[j + 1].name) > 0) {
-                swap(students[j], students[j + 1]);
-            }
-        }
-    }
-}
-
-void selectionSortByCGPA(Student* students, int size, bool ascending = true) {
-    for (int i = 0; i < size - 1; i++) {
-        int target = i;
-        for (int j = i + 1; j < size; j++) {
-            if ((ascending && students[j].cgpa < students[target].cgpa) ||
-                (!ascending && students[j].cgpa > students[target].cgpa))
-                target = j;
-        }
-        swap(students[i], students[target]);
-    }
-}
-
 int main() {
-    Student* students = nullptr;
+    Student students[100];
     int size = 0, choice;
 
     do {
-        cout << "\n1. Add Student\n2. Display\n3. Sort by Name\n4. Sort by CGPA (Asc)\n5. Sort by CGPA (Desc)\n6. Exit\nChoice: ";
+        cout << "\n1.Add 2.Display 3.Sort Name 4.Sort CGPA Asc 5.Sort CGPA Desc 6.Exit\nChoice: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1: addStudent(students, size); break;
-            case 2: displayStudents(students, size); break;
-            case 3: bubbleSortByName(students, size); break;
-            case 4: selectionSortByCGPA(students, size, true); break;
-            case 5: selectionSortByCGPA(students, size, false); break;
+        if(choice == 1) {
+            cout << "ID: "; cin >> students[size].id;
+            cout << "Name: "; cin >> students[size].name;
+            cout << "CGPA: "; cin >> students[size].cgpa;
+            size++;
         }
-    } while (choice != 6);
+        else if(choice == 2) {
+            cout << "\nID\tName\tCGPA\n";
+            for(int i=0;i<size;i++)
+                cout << students[i].id << "\t" << students[i].name << "\t" << students[i].cgpa << endl;
+        }
+        else if(choice == 3) { // Bubble sort by name
+            for(int i=0;i<size-1;i++)
+                for(int j=0;j<size-i-1;j++)
+                    if(strcmp(students[j].name, students[j+1].name)>0)
+                        swap(students[j], students[j+1]);
+        }
+        else if(choice == 4) { // Selection sort CGPA Asc
+            for(int i=0;i<size-1;i++){
+                int min_idx=i;
+                for(int j=i+1;j<size;j++)
+                    if(students[j].cgpa<students[min_idx].cgpa) min_idx=j;
+                swap(students[i], students[min_idx]);
+            }
+        }
+        else if(choice == 5) { // Selection sort CGPA Desc
+            for(int i=0;i<size-1;i++){
+                int max_idx=i;
+                for(int j=i+1;j<size;j++)
+                    if(students[j].cgpa>students[max_idx].cgpa) max_idx=j;
+                swap(students[i], students[max_idx]);
+            }
+        }
 
-    free(students);
-    return 0;
+    } while(choice != 6);
 }
